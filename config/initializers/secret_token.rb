205cc568f8bc69f8d15a3dbe81e283081afecdfb
Bob Schubert <bobschubert@gmail.com>
2013-03-31 08:40:16 -0700

    Initial Commit
    Installed spree shopping cart to new storefront.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bendtrailers::Application.config.secret_token = '0dae3142608ae68557ab032f6f100d239269d963c9308df4bf9f469ffed121e985e6f137b3b9125cce7b747070ce48857ac25696c63380e2e3242570b0458482'
