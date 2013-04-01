1a42c1c1302309cc693b15f4079f1627f9b7cb19
Bob Schubert <bobschubert@gmail.com>
2013-04-01 11:32:17 -0700

    changed logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,8 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.site_name = "Bend Trailers"
+  config.logo="store/bend_trailers_logo.gif"
 end
 
 Spree.user_class = "Spree::User"
